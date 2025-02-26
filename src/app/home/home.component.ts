e68f1e7cb3d968122d79de9f37204581d9d4284c
Mihajlo Markovic <mihajlomarkovic003@gmail.com>
2025-02-26 04:43:56 -0800

    Kraj dana 4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,12 @@ import { FlightService } from '../../services/flight.service';
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { AxiosError } from 'axios';
 import { FlightModel } from '../../models/flight.model';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe, NgIf, NgFor],
+  imports: [NgIf, NgFor, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -16,9 +18,19 @@ export class HomeComponent {
     public error : string | null = null
 
     constructor(){
-      FlightService.getFlights()
+      FlightService.getFlights(0, 3)
       .then(rsp => this.flights = rsp.data.content)
       .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
+    }
+
+    public formatDate(iso : string){
+
+      return new Date(iso).toLocaleString('sr-RS')
+
+    }
+
+    public generateDestionationImage(dest : string){
+      return `https://img.pequla.com/destination/${dest.split(' ')[0].toLowerCase()}.jpg`
     }
 
 }
